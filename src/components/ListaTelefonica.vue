e2a4d81645abc426bf13e31e228066a35f0acb89
Fernando Barreto da Costa <f.costa@accesscredit.com.br>
2020-10-05 17:32:56 -0400

    Iniciando projeto no vue, instalando axios, configurando a tela de listagem de contatos, configurando chamada de url

---
+++ src/components/ListaTelefonica.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3>Lista de contatos</h3>
+    <br />
+    <div v-if="visible">
+       <div v-for="contato in contatos" :key="contato.id">
+       <p>{{ contato.nome }}</p>
+    </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { Axios } from './common/http-common'
+
+export default {
+  data () {
+    return {
+      contatos: [],
+      visible: false
+    }
+  },
+  mounted () { this.listarContatos() },
+  methods: {
+    listarContatos () {
+      Axios.get('/contatos')
+        .then((response) => {
+          this.contatos = response.data
+          this.visible = true
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
